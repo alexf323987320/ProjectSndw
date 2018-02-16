ba0e4dd716e434398b86aeff8c17071686d52bf7
Alex <alexf323987320@gmail.com>
2018-02-16 17:27:56 +0700

    Json utils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,12 +2,46 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
 
-        return null;
-        //
+        if (json.isEmpty()) return null;
+
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONObject name = root.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            List<String> alsoKnownAs = jsonArrayToArrayList(name.getJSONArray("alsoKnownAs"));
+            String placeOfOrigin = root.getString("placeOfOrigin");
+            String description = root.getString("description");
+            String image = root.getString("image");
+            List<String> ingredients = jsonArrayToArrayList(root.getJSONArray("ingredients"));
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static ArrayList<String> jsonArrayToArrayList(JSONArray jsonArray) throws JSONException{
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++){
+            String element = jsonArray.getString(i);
+            result.add(element);
+        }
+
+        return result;
 
     }
 }
